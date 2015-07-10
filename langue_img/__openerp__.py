0d7c6331a5bc4cdd35c2957e8337ae867b9029c5
Yann Le Goff <ylegoff@atout-serenite.com>
2015-07-10 15:46:33 +0200

    Création du module de Gestion des langues (Drapeaux)

---
+++ langue_img/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Gestion des Langues / Drapeau (Menu Website)",
+
+    'summary': """
+        Gestionnaire de Menu des Langues avec Images
+    """,
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Yann LE GOFF Atout-Serenite.com",
+    'website': "http://www.Atout-Serenite.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'website',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['website'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],    
+    'installable': True,
+    'application': True,
+}
---
+++ langue_img/models/models.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+class Langueimg(models.Model):
+    _name = 'langueimg'
+
+    def urlimage(img):
+    	return 1
