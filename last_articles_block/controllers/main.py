9d9772657dbefef58db85437993e0d6a3e1f99c4
David Mills <dmills@atout-serenite.com>
2015-07-16 22:14:17 +0200

    Cleaning up last_articles block

--- last_articles_block/controllers/main.py
+++ last_articles_block/controllers/main.py
@@ -5,20 +5,31 @@ import pprint
 import base64
 
 class snippet_latest_posts_controller(http.Controller):
+
+
+    #
+    # Render the 3 articles deposited before the one specified 
+    # or the last 3 articles written if the page isn't a standard webpage
+    # 
     @http.route(['/snippet_latest_posts/render/<base>/<int:page>'], type='http', auth='public', website=True)
     def render_latest_posts(self, page, base, template='snippet_last_articles.liste_article', **post):
         cr, uid, context = request.cr, request.uid, request.context
         page_obj = request.registry['ir.ui.view']
         
         if base == 'ir.ui.view': 
-            page_ids = page_obj.search(cr, uid, [('page','=','True'),('id','<',page),('active','=','True')], limit=3, order='create_date desc', context=context)
+            domain = [('page','=','True'),('id','<',page),('active','=','True')]
+            page_ids = page_obj.search(cr, uid, domain, limit=3, order='create_date desc', context=context)
         else : 
-            page_ids = page_obj.search(cr, uid, [('page','=','True'),('active','=','True')], limit=3, order='create_date desc', context=context)
+            domain  [('page','=','True'),('active','=','True')]
+            page_ids = page_obj.search(cr, uid, domain, limit=3, order='create_date desc', context=context)
         
         posts = page_obj.browse(cr, uid, page_ids, context=context)
 
         return request.render("last_articles_block.liste_last", {'posts': posts})
         
+    #
+    # 
+    #
     @http.route(['/snippet_latest_posts/updimage/<base>/<int:page>'], type='http', auth='public', methods=['POST','GET'], website=True)
     def array_latest_image(self, base, page, **args):
         cr, uid, context, registry = request.cr, request.uid, request.context, request.registry
