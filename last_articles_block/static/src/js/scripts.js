872e148aaa719c2213bfb25cdd7fa6526caded97
David Mills <dmills@atout-serenite.com>
2015-07-16 22:32:28 +0200

    Cleaning up code

--- last_articles_block/static/src/js/scripts.js
+++ last_articles_block/static/src/js/scripts.js
@@ -19,34 +19,8 @@
     clean:function(){
       this.$target.empty();
     },
-    // build: function(){
-    //   var self     = this,
-    //       limit    = self.$target.data("posts_limit"),
-    //       template = self.$target.data("template"),
-    //       loading  = self.$target.data("loading");
-
-    //   // if no data, then use defaults values
-    //   if(!limit)limit = 3; // Limit default
-    //   if(!template) template = 'snippet_last_articles.liste_article';
-      
-    //   // create the domain
-
-    //   // call posts
-    //   openerp.jsonRpc("/snippet_latest_posts/array", 'call',{
-    //     'template': template,
-    //     'limit': limit,
-    //   }).then(function(posts) {
-    //      // just print the posts
-    //      alert(posts)
-    //      $(posts).appendTo(self.$target);
-        
-    //   })
-    //   .fail(function(e) {
-    //     // debug in js console
-    //     return;
-    //   });
-    // },
     build: function() {
+
       var xhr = new XMLHttpRequest();
       // On défini ce qu'on va faire quand on aura la réponse
       xhr.onreadystatechange = function(){
