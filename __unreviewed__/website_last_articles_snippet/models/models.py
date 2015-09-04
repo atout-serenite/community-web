8710f7f57e54e4f43e7874dcc3c1251bb06cb2d4
Valeureux - florent <florent@valeureux.org>
2015-09-04 13:47:11 +0200

    [IMP] Website community-web merge

---
+++ __unreviewed__/website_last_articles_snippet/models/models.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models,api
+from openerp.osv import orm, osv, fields
+# class testmodule(models.Model):
+#     _name = 'testmodule.testmodule'
+
+#     name = fields.Char()
+
+class website_seo_metadata(osv.Model):
+    _inherit = 'ir.ui.view'
+
+
+    def get_slug(self, cr, uid, ids, name, args, context):
+
+        result = {}
+        for id in ids:
+            model_pool = self.pool.get("ir.model.data")
+            valeurid = model_pool.search(cr, uid, [('module','=','website'), ('res_id','=', id)])
+            if not valeurid:
+                raise ValueError('External ID not found in the system: %s' % (id))
+            # the sql constraints ensure us we have only one result
+            res = model_pool.read(cr, uid, valeurid[0], ['name'])
+            if not res['name']:
+                raise ValueError('External ID not found in the system: %s' % (id))
+            result[id] = res['name']
+            
+        return result
+
+
+    _columns = {
+        'website_meta_image': fields.binary("Image"),
+        'slug': fields.function(get_slug, string='Slug', type='char'),
+    }
---
+++ __unreviewed__/website_last_articles_snippet/static/src/js/last_articles_snippet_display.js
@@ -0,0 +1,65 @@
+(function(){
+  'use strict';
+
+  var website = openerp.website;
+  var choixmethode = '2';
+
+
+
+  website.add_template_file('/website_last_articles_snippet/static/src/xml/lasts_articles_blocks.front.xml');
+
+  website.snippet.animationRegistry.js_get_posts = website.snippet.Animation.extend({
+    selector : ".last_articles_snippets",
+    getMainObject: function () {
+        var repr = $('html').data('main-object');
+        var m = repr.match(/(.+)\((\d+),(.*)\)/);
+        if (!m) {
+            return null;
+        } else {
+            return {
+                model: m[1],
+                id: m[2]|0
+            };
+        }
+    },
+
+    start: function(){
+      this.clean();
+      this.build();
+    },
+    stop: function(){
+      this.clean();
+    },
+    clean:function(){
+      this.$target.empty();
+    },
+    build: function() {
+      var mainObject = this.getMainObject();
+
+      var self = this;
+
+      var domain = [['page','=','True'],['active','=','True']]
+
+      if (mainObject[1] == 'ir.ui.view')
+      {
+        domain.push(['id','<',mainObject[2]]);
+      }
+
+      openerp.jsonRpc('/web/dataset/call_kw', 'call', {
+                model: 'ir.ui.view',
+                method: 'search_read',
+                args: [],
+                kwargs: {
+                    fields: ['slug', 'website_meta_image', 'website_meta_title', 'website_meta_description' , 'name'],
+                    domain: domain,
+                    order: 'create_date desc',
+                    limit: 3
+                }
+            }).then( function(data) {
+                self.$el.append(openerp.qweb.render("website_last_articles_snippet.liste_articles", {"posts": data}));
+            });
+    }
+
+  })
+
+})();
